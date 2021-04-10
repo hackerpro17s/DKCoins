74810e9e8b8ec4fea913b60379b0b40f7936504a
Fridious <philippfriedhoff@outlook.de>
2021-04-10 18:29:27 +0200

    build trigger

--- dkcoins-minecraft/src/main/java/net/pretronic/dkcoins/minecraft/integration/DKCoinsPlaceholderHook.java
+++ dkcoins-minecraft/src/main/java/net/pretronic/dkcoins/minecraft/integration/DKCoinsPlaceholderHook.java
@@ -52,7 +52,6 @@ public class DKCoinsPlaceholderHook implements PlaceholderHook {
     dkcoins_bank_(bank)_balance_[currency]
      */
 
-
     @Override
     public Object onRequest(MinecraftPlayer player, String parameter) {
         String[] parameters = parameter.toLowerCase().split("_");
